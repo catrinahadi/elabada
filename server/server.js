0bd9fe223c690454f9402151fe5842558840d371
catrinahadi <mohadi1@up.edu.ph>
2026-03-08 21:55:59 +0800

    added server check for deployment

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("ELaBada API is running...");
+});
+
 // Serve uploaded files statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
